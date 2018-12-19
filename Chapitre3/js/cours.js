afc3389b35bc90d5c56cd0899b3d9eef716ee6a2
thpGitHub <thierrypoupon@hotmail.com>
2018-12-19 21:04:58 +0100

    cours du chapitre 3

--- Chapitre3/js/cours.js
+++ Chapitre3/js/cours.js
@@ -12,4 +12,24 @@ titreElt.classList.remove("debut");// Suppression de la classe "debut"
 titreElt.classList.add("titre");// Ajout de la classe "titre"
 console.log(titreElt);
 
-// commentaire pour github
+var pythonElt = document.createElement("li"); // Création d'un élément li
+pythonElt.id = "python"; // Définition de son identifiant
+pythonElt.textContent = "Python"; // Définition de son contenu textuel
+document.getElementById("langages").appendChild(pythonElt); // Insertion du nouvel élément
+
+var rubyElt = document.createElement("li"); // Création d'un élément li
+rubyElt.id = "ruby"; // Définition de son identifiant
+rubyElt.appendChild(document.createTextNode("Ruby")); // Définition de son contenu textuel
+document.getElementById("langages").appendChild(rubyElt); // Insertion du nouvel élément
+
+var perlElt = document.createElement("li"); // Création d'un élément li
+perlElt.id = "perl"; // Définition de son identifiant
+perlElt.textContent = "Perl"; // Définition de son contenu textuel
+// Ajout du nouvel élément avant l'identifiant identifié par "php"
+document.getElementById("langages").insertBefore(perlElt, 
+    document.getElementById("php"));
+
+
+// Ajout d'un élément au tout début de la liste
+document.getElementById('langages').insertAdjacentHTML("afterBegin", 
+    '<li id="javascript">JavaScript</li>');
