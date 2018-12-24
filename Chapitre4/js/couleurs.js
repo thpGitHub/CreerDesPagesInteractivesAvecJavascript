20aaf4fb1edae670eab77c12565f48f8e1b4d76f
thpGitHub <thierrypoupon@hotmail.com>
2018-12-25 00:13:18 +0100

    chapitre4

---
+++ Chapitre4/js/couleurs.js
@@ -0,0 +1,11 @@
+var couleurText=prompt("Veuillez entrer une couleur");
+var couleurFond=prompt("Veuillez entrer une couleur de fond");
+
+var divElt=document.getElementsByTagName("div");
+
+for (var i=0; i<divElt.length;i++){
+		divElt[i].style.backgroundColor=couleurFond;
+		divElt[i].style.color=couleurText;
+
+
+}
---
+++ Chapitre4/js/cours.js
@@ -0,0 +1,6 @@
+var pElt=document.querySelector("p");
+pElt.style.color="red";
+pElt.style.margin="50px";
+
+pElt.style.fontFamily="arial";
+pElt.style.backgroundColor="black";
